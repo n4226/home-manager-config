cddcd586d5a01447f39b246c913a12f905b2c199
Michael Baron <developer.michael.baron@gmail.com>
2025-04-26 14:43:18 -0400

    small change

--- home.nix
+++ home.nix
@@ -32,23 +32,23 @@
     plugins = with pkgs.tmuxPlugins; [
       catppuccin
     ];
-    extraConfig = ''
-		    # set-option -g status-position top
-		    # bind -n Super_L send-keys "A-j"
-		     \'\';
-		     #   # bind -n D-j send-keys "A-j"
-		     #   # bind -n D-k send-keys "A-k"
-		     # \'\';
-		     # 1) allow tmux to see and pass through Meta-modifiers
-		     set -g xterm-keys on
-
-		# 2) bind M-j and M-k at the tmux level to literally resend M-j/k
-		#    (unbind first in case tmux has a built-in binding)
-		unbind -n M-j
-		unbind -n M-k
-		bind -n M-j send-keys M-j
-		bind -n M-k send-keys M-k
-    '';
+		#   extraConfig = ''
+		#     # set-option -g status-position top
+		#     # bind -n Super_L send-keys "A-j"
+		#      \'\';
+		#      #   # bind -n D-j send-keys "A-j"
+		#      #   # bind -n D-k send-keys "A-k"
+		#      # \'\';
+		#      # 1) allow tmux to see and pass through Meta-modifiers
+		#      set -g xterm-keys on
+		#
+		# # 2) bind M-j and M-k at the tmux level to literally resend M-j/k
+		# #    (unbind first in case tmux has a built-in binding)
+		# unbind -n M-j
+		# unbind -n M-k
+		# bind -n M-j send-keys M-j
+		# bind -n M-k send-keys M-k
+		#   '';
   };
 
 
